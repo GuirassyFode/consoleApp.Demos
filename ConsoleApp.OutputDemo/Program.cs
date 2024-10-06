4a4a2be899a7c4dbd49660b1ae5c350da6496cae
Fode Guirassy <papeguirassy@gmail.com>
2024-10-05 23:41:28 -0400

    first commit

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! My name is Fode Guirassy!");
+
+Console.WriteLine("I am form Senegal, West Africa");
+
+Console.Write("Console1");
+Console.Write("Console2");
+Console.Write("Console3");
